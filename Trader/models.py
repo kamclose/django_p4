05cc67ec5818fabace5079e2968ef7c8dc40b59c
Kameron Close <44491221+kamclose@users.noreply.github.com>
2023-07-30 22:21:06 +0500

    Update models.py

--- Trader/models.py
+++ Trader/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    stock = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    value = models.DecimalField(decimal_places=2, max_digits=10)
